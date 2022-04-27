8ea480ed3b72ddfd2c8240c0f0a8c50aaf2f3ce1
Abhishek Kumar <59302147+2Abhi000@users.noreply.github.com>
2022-04-27 02:16:41 -0400

    Add files via upload

---
+++ Problem no. 167. Two Sum II - Input Array Is Sorted.py
@@ -0,0 +1,13 @@
+# Problem no. 167. Two Sum II - Input Array Is Sorted
+numbers = [-1,0]
+target = -1
+a=[]
+for i in range(len(numbers)):
+    for j in range(i+1,len(numbers)):
+        if(numbers[i]+numbers[j]==target):
+            a.append(i+1)
+            a.append(j+1)
+print(a)
+
+
+
